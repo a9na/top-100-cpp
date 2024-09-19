5ce6d14ebe20415388b75f12659a51cf67d309cc
Ana Novković <anaiscoding@gmail.com>
2024-09-19 09:31:14 +0200

    Create remove-all-char-except-alphabets.cpp

---
+++ remove-all-char-except-alphabets.cpp
@@ -0,0 +1,28 @@
+// C++ Programto remove all characters from a string except
+// alphabets
+#include <cctype>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string remove_non_alphabets(string str)
+{
+    string result = "";
+    for (char c : str) {
+        if (isalpha(c)) {
+            result += c;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    string str = "am1azing";
+
+    cout << "Alphabets only: " << remove_non_alphabets(str)
+         << endl;
+
+    return 0;
+}

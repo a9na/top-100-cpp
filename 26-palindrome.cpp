18cc8507639826f44a1c7fc57e93fab479c0fb85
Ana Novković <anaiscoding@gmail.com>
2024-10-19 16:03:04 +0200

    Create 26-palindrome.cpp

---
+++ 26-palindrome.cpp
@@ -0,0 +1,24 @@
+// C++ program for checking
+// if it is Palindrome or not
+#include <iostream>
+using namespace std;
+
+string isPalindrome(string S)
+{
+    for (int i = 0; i < S.length() / 2; i++) {
+        if (S[i] != S[S.length() - i - 1]) {
+            return "No";
+        }
+    }
+
+    return "Yes";
+}
+
+int main()
+{
+    string S = "Anuska";
+
+    cout << isPalindrome(S);
+
+    return 0;
+}
